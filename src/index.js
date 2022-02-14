77d02ce5a6656c40c0c9ba76e9c23fe8caf01eb0
Rob Relyea <rob@relyeas.net>
2022-02-14 08:21:29 -0800

    add links to treatment locator sites

--- src/index.js
+++ src/index.js
@@ -312,7 +312,8 @@ function renderPage(states, evusheldSites, dataUpdates) {
           <div style={styles.smallerFont}>&nbsp;</div>
           <div style={styles.smallerFont}>
             Contact: <a href="https://twitter.com/rrelyea">@rrelyea</a> or <a href="mailto:rob@relyeas.net">rob@relyeas.net</a> | 
-            Github repo for <a href="https://github.com/rrelyea/evusheld">this site</a> and <a href="https://github.com/rrelyea/evusheld-locations-history">data fetching</a>
+            Github repo for <a href="https://github.com/rrelyea/evusheld">this site</a> and <a href="https://github.com/rrelyea/evusheld-locations-history">data fetching</a> |
+            Treament locators: <a href="https://rrelyea.github.io/sotrovimab">sotrovimab</a> and <a href="https://rrelyea.github.io/paxlovid">paxlovid</a>
           </div>
         </div>
       </div>
