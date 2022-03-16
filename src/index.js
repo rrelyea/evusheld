523e1ec694ff481e0b4fdcab662c6490558df9d3
Rob Relyea <rob@relyeas.net>
2022-03-16 16:59:29 -0700

    add ? to buy me a coffee

--- src/index.js
+++ src/index.js
@@ -566,7 +566,7 @@ function renderPage(states, mabSites) {
           : false }
           <div style={styles.smallerFont}>&nbsp;</div>
           <div style={styles.smallerCentered}>
-          <b>Why:</b> <a href='https://www.geekwire.com/2022/after-wife-got-cancer-microsoft-engineer-built-a-tool-to-locate-anti-covid-drug-for-immunocompromised/'>why I built this site</a> <b>Sponsor:</b> <a href='https://buymeacoffee.com/rrelyea'>buy me a coffee</a> <b>Contact:</b> <a href="https://twitter.com/rrelyea">twitter/rrelyea</a>, <a href="mailto:rob@relyeas.net">rob@relyeas.net</a> <b>Open source:</b> <a href={"https://github.com/rrelyea/"+constants.site.toLowerCase()}>{'/'+ constants.siteLower}</a>, <a href="https://github.com/rrelyea/evusheld-locations-history">git-scraping</a>
+          <b>Why:</b> <a href='https://www.geekwire.com/2022/after-wife-got-cancer-microsoft-engineer-built-a-tool-to-locate-anti-covid-drug-for-immunocompromised/'>why I built this site</a> <b>Sponsor:</b> <a href='https://buymeacoffee.com/rrelyea'>buy me a coffee?</a> <b>Contact:</b> <a href="https://twitter.com/rrelyea">twitter/rrelyea</a>, <a href="mailto:rob@relyeas.net">rob@relyeas.net</a> <b>Open source:</b> <a href={"https://github.com/rrelyea/"+constants.site.toLowerCase()}>{'/'+ constants.siteLower}</a>, <a href="https://github.com/rrelyea/evusheld-locations-history">git-scraping</a>
           </div>
           <div style={styles.smallerCentered}>&nbsp;</div>
         </div>
